fd81d2a6a733ac1de148d0f378c4ba0cdc14386e
HAREESH <53457844+hareesh-r@users.noreply.github.com>
2020-08-20 22:24:45 +0530

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,31 @@
+<?php
+    $name =$_POST['name'];
+    $emailId =$_POST['emailid'];
+    $regno =$_POST['RegNo'];
+    $descri =$_POST['desc'];
+    $phno =$_POST['phno'];
+    $gpa =$_POST['cgpa'];
+
+    $email_from = 'hareeshraji7081@gmail.com';
+
+    $email_subject = "Application for alumini";
+
+    $email_body = "I am $name.\n". 
+                    "My Register Number is $regno.\n". 
+                     "Contact me at $phno.\n". 
+                      "My GPA $gpa.\n". 
+                       " $descri.\n". 
+                        "E-mail : $emailId.\n";
+
+    $to = "hareeshraji708@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers = "Reply-To: $emailId \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
+?>
