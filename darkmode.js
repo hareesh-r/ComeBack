5916e822d2f1e7198a9ea3c9abbbb510735e874a
HAREESH <53457844+hareesh-r@users.noreply.github.com>
2020-10-04 20:49:31 +0530

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,22 @@
+const options = {
+    bottom: '64px',
+    right: '32px',
+    left: 'unset',
+    time: '0.3s',
+    mixColor: '#fff',
+    backgroundColor: '#fff',
+    buttonColorDark: '#000',
+    buttonColorLight: '#fff',
+    saveInCookies: true,
+    label: '🌙',
+  }
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
+
+//   function darkmodebaba(params) {
+//     document.getElementById("navbar").style.color = "black";
+//   document.getElementById("nav-link").style.color = "white";
+// }
+// darkmodebaba.call();
